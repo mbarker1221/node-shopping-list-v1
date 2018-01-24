a2e132900cb944bfaee718b5030908283ba51d77
michelle barker <mbarker1221@gmail.com>
2018-01-23 22:36:23 -0500

    init commit with added recipes

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 
 const express = require('express');
+
+const router=express.Router();
 // we'll use morgan to log the HTTP layer
 const morgan = require('morgan');
 // we'll use body-parser's json() method to 
@@ -8,7 +10,8 @@ const bodyParser = require('body-parser');
 
 // we import the ShoppingList model, which we'll
 // interact with in our GET endpoint
-const {ShoppingList} = require('./models');
+const {ShoppingList, Recipes} = require('./models');
+
 
 const jsonParser = bodyParser.json();
 const app = express();
@@ -25,11 +28,20 @@ ShoppingList.create('beans', 2);
 ShoppingList.create('tomatoes', 3);
 ShoppingList.create('peppers', 4);
 
+Recipes.create('chocolate milk', ['cocoa', 'milk', 'sugar']);
+Recipes.create('oatmeal', ['oats', 'milk', 'sugar']);
+Recipes.create('cereal', ['cereal', 'milk']);
+
 // when the root of this route is called with GET, return
 // all current ShoppingList items by calling `ShoppingList.get()`
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
 });
+
+app.get('/recipes', (req, res) => {
+	res.json(Recipes.get());
+});
+
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
